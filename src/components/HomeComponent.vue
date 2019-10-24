6c19efa3ccb49a8118978c96766707e85c878631
Christopher Carlson <c.m.carlson13@gmail.com>
2019-10-23 22:27:56 -0600

    EOD

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,10 +1,20 @@
 <template>
   <v-container>
     <v-row style="height: 70vh" align="center">
-      <v-col justify-content="center" cols="1"
-      v-for="icon in icons"
-      :key="icon">
-        <i :class="icon"></i>
+      <v-col cols="12">
+        <v-row>
+          <h1>Hi, I'm Chris!</h1>
+        </v-row>
+        <v-row>
+          <h3>Full Stack Developer | CSS Enthusiast | </h3>
+        </v-row>
+        <v-row>
+          <v-col class="text-center" cols="1"
+          v-for="icon in icons"
+          :key="icon">
+            <i :class="icon"></i>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -15,11 +25,10 @@ export default {
   data: () => ({
     icons: [
     'fab fa-github fa-5x',
-    'fab fa-github fa-5x',
-    'fab fa-github fa-5x',
-    'fab fa-github fa-5x',
-    'fab fa-github fa-5x',
-    'fab fa-github fa-5x'
+    'fab fa-linkedin fa-5x',
+    'fab fa-twitter fa-5x',
+    'fas fa-folder-open fa-5x',
+    'fas fa-file fa-5x'
     ]
   }),
 };
