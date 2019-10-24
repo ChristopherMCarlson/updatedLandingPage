43692c4795699ad870f65901def4618840106198
Christopher Carlson <c.m.carlson13@gmail.com>
2019-10-23 22:11:17 -0600

    v-for working, justify being dumb

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>CHRIS</span>
+        <span class="font-weight-light">CARLSON</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -11,23 +11,23 @@
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Full Stack Developer</span>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <HomeComponent/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HomeComponent from './components/HomeComponent';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HomeComponent,
   },
   data: () => ({
     //
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row style="height: 70vh" align="center">
+      <v-col justify-content="center" cols="1"
+      v-for="icon in icons"
+      :key="icon">
+        <i :class="icon"></i>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: [
+    'fab fa-github fa-5x',
+    'fab fa-github fa-5x',
+    'fab fa-github fa-5x',
+    'fab fa-github fa-5x',
+    'fab fa-github fa-5x',
+    'fab fa-github fa-5x'
+    ]
+  }),
+};
+</script>
