68a30fe20879dd090b72499da22ac8236944258a
Christopher Carlson <c.m.carlson13@gmail.com>
2019-10-24 22:04:09 -0600

    can't get hover to work

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -2,17 +2,25 @@
   <v-container>
     <v-row style="height: 70vh" align="center">
       <v-col cols="12">
-        <v-row>
+        <v-row class='d-flex justify-center'>
           <h1>Hi, I'm Chris!</h1>
         </v-row>
-        <v-row>
-          <h3>Full Stack Developer | CSS Enthusiast | </h3>
+        <v-row class='d-flex justify-center'>
+          <h3>Full Stack Developer | CSS Enthusiast | Material Designer</h3>
         </v-row>
-        <v-row>
+        <v-row class='d-flex justify-center'>
           <v-col class="text-center" cols="1"
           v-for="icon in icons"
           :key="icon">
-            <i :class="icon"></i>
+            <v-hover v-slot:default="{ hover }">
+              <v-icon
+              :href="icon.link"
+              size="100"
+              tag="a"
+              target="_blank"
+              style="text-decoration: none;"
+              :color="hover ? grey : blue">{{icon.icon}}</v-icon>
+            </v-hover>
           </v-col>
         </v-row>
       </v-col>
@@ -24,11 +32,26 @@
 export default {
   data: () => ({
     icons: [
-    'fab fa-github fa-5x',
-    'fab fa-linkedin fa-5x',
-    'fab fa-twitter fa-5x',
-    'fas fa-folder-open fa-5x',
-    'fas fa-file fa-5x'
+    {
+      icon: 'fab fa-github fa-5x',
+      link: 'https://github.com/ChristopherMCarlson'
+    },
+    {
+      icon: 'fab fa-linkedin fa-5x',
+      link: 'https://www.linkedin.com/in/christopher-carlson-303953163/'
+    },
+    {
+      icon: 'fab fa-twitter fa-5x',
+      link: 'https://twitter.com/C_M_Carlson'
+    },
+    {
+      icon: 'fas fa-folder fa-5x',
+      link: 'https://cmcinspire.herokuapp.com/#/main'
+    },
+    {
+      icon: 'fas fa-file fa-5x',
+      link: 'https://docs.google.com/document/d/19JO6IoMKg1Fyhq4Tli8sAsapy7Ds-EGa5JL3VC-NEFk/edit?usp=sharing'
+    }
     ]
   }),
 };
