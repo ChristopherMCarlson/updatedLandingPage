516b0b646b9841af09ed5c64596bfca1b6a4e6e8
Christopher Carlson <c.m.carlson13@gmail.com>
2019-10-28 22:12:09 -0600

    resume is looking good on desktop

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,17 +1,29 @@
 <template>
     <v-container>
       <v-row style="height: 70vh" justify="center">
-        <v-col cols="10">
-          <h1 class="text-center">Christopher Carlson</h1>
+        <v-col cols="6">
+          <p class="text-center display-1">Christopher Carlson</p>
           <hr/>
           <v-row>
             <v-col cols="12">
-              <p class='text-left'>c.m.carlson13@gmail.com</p>
-              <p class='text-center'>208-813-5288</p>
-              <p class='text-right'>github.com/christophermcarlson</p>
+              <v-row class='d-flex justify-space-between'>
+                <p>c.m.carlson13@gmail.com</p>
+                <p>208-813-5288</p>
+              </v-row>
             </v-col>
           </v-row>
-          <h4 class="text-center">SOFTWARE ENGINEER</h4>
+          <p class="text-center headline">SOFTWARE ENGINEER</p>
+          <p class="text-left body-1">A US Army veteran with experience in front and back-end software development technologies. Passionate about innovation, problem solving and building applications with an emphasis on client functionality. Recently completed an immersive coding program, challenged and grew in programming and scrum skills. A goal setter, natural born leader and possesses excellent collaboration and project coordinating abilities. Seeking an opportunity with a leading tech company focused on connecting users with creative breakthrough applications.</p>
+          <v-list disabled>
+            <v-list-item-content>
+              <v-list-item-title class='title'>Innovative Software Engineer</v-list-item-title>
+              <p class='body-1'>Offering experience in the full software development lifecycle- from concept through delivery of applications and customizable solutions.</p>
+            <v-list-item-title class='title'>Experienced in development methodologies,</v-list-item-title>
+            <p class='body-1'>tools and processes contributing to the design and rollout of software applications.</p>
+            <v-list-item-title class='title'>Known for excellent troubleshooting skills</v-list-item-title>
+            <p class='body-1'>Able to analyze code and engineer well-researched responsive solutions.</p>
+            </v-list-item-content>
+          </v-list>
         </v-col>
       </v-row>
     </v-container>
