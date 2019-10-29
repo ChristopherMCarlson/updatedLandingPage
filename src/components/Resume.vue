47d2bb8eda9b74bb140389dd7dba9742ca1f217a
Christopher Carlson <c.m.carlson13@gmail.com>
2019-10-28 21:37:46 -0600

    text alignment is hard

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -2,7 +2,16 @@
     <v-container>
       <v-row style="height: 70vh" justify="center">
         <v-col cols="10">
-          <h1>Christopher Carlson</h1>
+          <h1 class="text-center">Christopher Carlson</h1>
+          <hr/>
+          <v-row>
+            <v-col cols="12">
+              <p class='text-left'>c.m.carlson13@gmail.com</p>
+              <p class='text-center'>208-813-5288</p>
+              <p class='text-right'>github.com/christophermcarlson</p>
+            </v-col>
+          </v-row>
+          <h4 class="text-center">SOFTWARE ENGINEER</h4>
         </v-col>
       </v-row>
     </v-container>
