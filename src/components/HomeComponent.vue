58183202c128d161dc2abf736f52af13d29092f6
Christopher Carlson <c.m.carlson13@gmail.com>
2019-11-11 13:50:33 -0700

    its working

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -71,7 +71,7 @@
     <v-row justify="center">
       <v-dialog v-model="resumeDialog" fullscreen hide-overlay="true" transition="dialog-bottom-transition" scrollable="true">
         <v-card>
-          <v-toolbar>
+          <v-toolbar elevation='0'>
             <v-btn icon dark @click="resumeDialog = false">
               <v-icon color='grey'>mdi-close</v-icon>
             </v-btn>
@@ -81,8 +81,78 @@
               <v-btn color="blue" text @click="resumeDialog = false">Download</v-btn>
             </v-toolbar-items>
           </v-toolbar>
-          <v-card-text>
-            
+          <v-card-text class='mt-3 justify-center' style="padding: 0% 25% 0% 25%">
+            <p class="text-center display-1">Christopher Carlson</p>
+            <hr/>
+            <v-row>
+              <v-col cols="12">
+                <v-row class='d-flex justify-space-between'>
+                  <p>c.m.carlson13@gmail.com</p>
+                  <p>208-813-5288</p>
+                </v-row>
+              </v-col>
+            </v-row>
+            <p class="text-center headline">SOFTWARE ENGINEER</p>
+            <p class="text-left body-1">A US Army veteran with experience in front and back-end software development technologies. Passionate about innovation, problem solving and building applications with an emphasis on client functionality. Recently completed an immersive coding program, challenged and grew in programming and scrum skills. A goal setter, natural born leader and possesses excellent collaboration and project coordinating abilities. Seeking an opportunity with a leading tech company focused on connecting users with creative breakthrough applications.</p>
+            <v-list disabled>
+              <v-list-item-content>
+                <v-list-item-title class='title'>Innovative Software Engineer</v-list-item-title>
+                <p class='body-1'>Offering experience in the full software development lifecycle- from concept through delivery of applications and customizable solutions.</p>
+                <v-list-item-title class='title'>Experienced in development methodologies,</v-list-item-title>
+                <p class='body-1'>tools and processes contributing to the design and rollout of software applications.</p>
+                <v-list-item-title class='title'>Known for excellent troubleshooting skills</v-list-item-title>
+                <p class='body-1'>Able to analyze code and engineer well-researched responsive solutions.</p>
+              </v-list-item-content>
+            </v-list>
+            <hr/>
+          <p class="text-center headline">PROFESSIONAL EXPERIENCE</p>
+          <v-row class='d-flex justify-space-between'>
+            <p class="subtitle-1 font-weight-medium">Junior Software Engineer, Verified First</p>
+            <p class="subtitle-1 font-weight-medium">11/2018-Present</p>
+          </v-row>
+          <v-list disabled>
+            <v-list-item-content>
+              <p class='body-1'>&#8226; Part of a three person development team in charge of internal software development.</p>
+              <p class='body-1'>&#8226; Met with internal employees to discover, plan, and develop solutions to optimize workflow</p>
+              <p class='body-1'>&#8226; Conducted QA for internal design and development</p>
+              <p class='body-1'>&#8226; Administered hotfixes to ensure smooth daily operations</p>
+              <p class='body-1'>&#8226; Served as a thought partner for the Director of Information Systems</p>
+              <p class='body-1'>&#8226; Programmed back-end and front-end code within SalesForce</p>
+            </v-list-item-content>
+          </v-list>
+          <v-row class='d-flex justify-space-between'>
+              <p class="subtitle-1 font-weight-medium">Full Stack Developer Student, CodeWorks</p>
+              <p class="subtitle-1 font-weight-medium">07/2018-10/2018</p>
+            </v-row>
+            <v-list disabled>
+              <v-list-item-content>
+                <p class='body-1'>&#8226; Completed over 500 hours of software development in an immersive learning environment.</p>
+                <p class='body-1'>&#8226; Participated in 24-hour hack-a-thons events testing and challenging personal goals.</p>
+                <p class='body-1'>&#8226; Designed a music playlist app (Vue Music) where users search and add favorite artists to personalized lists. Integrated with iTunes Api, and built with VueJS.</p>
+                <p class='body-1'>&#8226; Utilized VueJS, node, and Vuetify to develop a user dashboard app displaying current weather, time, and rotating quotes and images.</p>
+                <p class='body-1'>&#8226; Collaborated on group capstone project (Lendr), created an app to track status of loaned items to other users and allowed users to confirm receipt of lent items with attached due dates. Developed with VueJS, Vuetify, and Socket.io.</p>
+                <p class='body-1'>&#8226; Collaborated on a group project (Post-It) during 24-hour hack-a-thon to create a messaging board that allowed users to create accounts, start discussion threads, comment on threads, and provide feedback on comments and posts via a voting system.</p>
+                <p class='body-1'>&#8226; Spent time learning Vuetify for VueJS in order to better style web apps.</p>
+              </v-list-item-content>
+            </v-list>
+            <v-row class='d-flex justify-space-between'>
+              <p class="subtitle-1 font-weight-medium"></p>
+              <p class="subtitle-1 font-weight-medium"></p>
+            </v-row>
+            <v-list disabled>
+              <v-list-item-content>
+                <p class='body-1'>&#8226; </p>
+              </v-list-item-content>
+            </v-list>
+            <v-row class='d-flex justify-space-between'>
+              <p class="subtitle-1 font-weight-medium"></p>
+              <p class="subtitle-1 font-weight-medium"></p>
+            </v-row>
+            <v-list disabled>
+              <v-list-item-content>
+                <p class='body-1'>&#8226; </p>
+              </v-list-item-content>
+            </v-list>
           </v-card-text>
         </v-card>
       </v-dialog>
